cb4e70edb82a19a187c63eb328a120fa6f3f1304
Fabián <fanavar@alumnos.ubiobio.cl>
2023-06-22 22:01:32 -0400

    Initial commit

---
+++ app/src/main/java/cl/awakelab/ejercicio8/MainActivity.java
@@ -0,0 +1,31 @@
+package cl.awakelab.ejercicio8;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initListeners();
+    }
+
+    private void initListeners(){
+        Button secondActivityButton = findViewById(R.id.secondActivityButton);
+        secondActivityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    Intent intent = new Intent( MainActivity.this, SecondActivity.class);
+                    startActivity(intent);
+            }
+        });
+
+
+    }
+}
---
+++ app/src/main/java/cl/awakelab/ejercicio8/SecondActivity.java
@@ -0,0 +1,29 @@
+package cl.awakelab.ejercicio8;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        initListeners();
+    }
+    private void initListeners(){
+        Button goToUrlButton = findViewById(R.id.goToUrlButton);
+        goToUrlButton.setOnClickListener(new View.OnClickListener(){
+           @Override
+           public void onClick(View v){
+               Intent implicitIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://kotlindoc.blogspot.com/2019/10/android-log-con-timber.html"));
+           startActivity(implicitIntent);
+           }
+        });
+    }
+}
