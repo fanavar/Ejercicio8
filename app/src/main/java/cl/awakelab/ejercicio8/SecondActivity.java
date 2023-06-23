8d07acbb9b7af54b6c994cf2459850bc47be9b9a
Fabián <fanavar@alumnos.ubiobio.cl>
2023-06-22 22:28:21 -0400

    Ejercicio 8 terminado

--- app/src/main/java/cl/awakelab/ejercicio8/SecondActivity.java
+++ app/src/main/java/cl/awakelab/ejercicio8/SecondActivity.java
@@ -18,12 +18,20 @@ public class SecondActivity extends AppCompatActivity {
     }
     private void initListeners(){
         Button goToUrlButton = findViewById(R.id.goToUrlButton);
+        Button buttonBack = findViewById(R.id.buttonBack);
         goToUrlButton.setOnClickListener(new View.OnClickListener(){
            @Override
            public void onClick(View v){
                Intent implicitIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://kotlindoc.blogspot.com/2019/10/android-log-con-timber.html"));
            startActivity(implicitIntent);
            }
+        });
+
+        buttonBack.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                onBackPressed();
+            }
         });
     }
 }
